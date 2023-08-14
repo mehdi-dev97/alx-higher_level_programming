91785f062ff3a13eba0b54fc5ec86634e45a53f7
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-08-14 16:10:34 +0100

    Remove [c-C] form string without using any builin func

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-	new_string = my_string.translate({ord(i): None for i in 'cC'})
+	new_string = ''
+	for i in my_string:
+		if i != 'c' and i != 'C':
+			new_string += i
 	return new_string
