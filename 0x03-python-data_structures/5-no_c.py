d047ed4d749c634467c0d03b4248dc665c825f0a
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-08-14 14:59:48 +0000

    Remove c and C form string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-	new_string = my_string.translate({ord("c"): None})
-	new_string = new_string.translate({ord("C"): None})
+	new_string = my_string.translate({ord(i): None for i in 'cC'})
 	return new_string
