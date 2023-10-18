d0cd888a4e3afd4bcf483f3bff2549aba0907b89
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-10-18 21:19:07 +0100

    List of all cities in California state

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- List of all cities in California state
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
