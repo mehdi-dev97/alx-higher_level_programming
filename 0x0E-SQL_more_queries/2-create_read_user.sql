ee7697eee636c3b4702470a9fe10283d5adbf66b
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-10-18 14:49:32 +0100

    Create user_0d_2 and database hbtn_0d_2 and set only Select privilege on this user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create user_0d_2 and database hbtn_0d_2 and set only Select privilege on this user
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2` CHARSET='utf8';
-GRANT SELECT on `hbtn_0d_2`.* TO `user_0d_2`@`localhost` WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
