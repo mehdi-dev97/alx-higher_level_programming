bdccbc105075d18ab90633f2c547c6b24e478e1a
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-11-13 12:17:11 +0100

    script that prints x times (passed in argv) “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,11 +3,11 @@
 const argv = process.argv;
 const numberOfCount = parseInt(argv[2]);
 if (!isNaN(numberOfCount)) {
-    let i = 0;
-    while (i < numberOfCount) {
-        console.log('C is fun');
-        i++;
-    }
+  let i = 0;
+  while (i < numberOfCount) {
+    console.log('C is fun');
+    i++;
+  }
 } else {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
