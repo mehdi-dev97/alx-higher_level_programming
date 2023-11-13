cac077b0e3bd0fdd5a61370eb3600d26a82c631f
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-11-13 13:57:57 +0100

    Fix coding style error in returned assign 103-object_fct.js.

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = () => myObject.value += 1;
+myObject.incr = () => myObject.value++;
 myObject.incr();
 console.log(myObject);
 myObject.incr();
