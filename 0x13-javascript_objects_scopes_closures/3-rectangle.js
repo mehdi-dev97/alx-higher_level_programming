abe4d5832f83bf8ce55d55d568031fd0757efb0d
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-11-14 13:33:41 +0100

    Fix style code error on rectangle class.

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.with = w;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    for (let x = 0; x < this.height; x++) {
+    for (let i = 0; i < this.height; i++) {
       let s = '';
-      for (let y = 0; y < this.with; y++) {
+      for (let j = 0; j < this.width; j++) {
         s += 'X';
       }
       console.log(s);
     }
   }
-};
+}
+
+module.exports = Rectangle;
