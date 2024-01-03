9547959a20dc540233369bd09204a343728e977f
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2024-01-03 11:23:55 +0100

    script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const argv = process.argv;
 
 fs.readFile(argv[2], 'utf8', function (error, content) {
-    console.log(error || content);
+  console.log(error || content);
 });
